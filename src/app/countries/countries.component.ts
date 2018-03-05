14788005392db5caf99d45077e7e32d262346749
Alaa Eddine <alaa.bouhajdeb@gmail.com>
2018-03-05 12:40:32 +0100

    Fixed conflicts

--- src/app/countries/countries.component.ts
+++ src/app/countries/countries.component.ts
@@ -1,12 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
-<<<<<<< HEAD
-import { Country} from '../models/country';
-import { COUNTRIES } from '../mocks/mock-countries';
-=======
-import { Country } from 'app/models/country'
-import { COUNTRIES } from 'app/mocks/mock-countries'
->>>>>>> 5b80b708bf86245c977e7cd248d2d048958b611e
+import { Country } from 'app/models/country';
+import { COUNTRIES } from 'app/mocks/mock-countries';
 
 @Component({
   selector: 'app-countries',
@@ -14,24 +9,15 @@ import { COUNTRIES } from 'app/mocks/mock-countries'
   styleUrls: ['./countries.component.css']
 })
 export class CountriesComponent implements OnInit {
-<<<<<<< HEAD
-    selectedCountry: Country;
-    countries: Country[] = COUNTRIES;
-=======
   selectedCountry: Country;
   countries: Country[] = COUNTRIES;
->>>>>>> 5b80b708bf86245c977e7cd248d2d048958b611e
 
   constructor() { }
 
   ngOnInit() {
   }
 
-<<<<<<< HEAD
-  onSelect(country: Country): void {
-=======
   onSelect(country: Country): void{
->>>>>>> 5b80b708bf86245c977e7cd248d2d048958b611e
     this.selectedCountry = country;
   }
 
