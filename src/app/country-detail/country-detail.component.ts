96bc66bb17d99441a174cb829ea05eed448869b7
Alessandro <lockit@lockit.it>
2018-03-05 15:17:02 +0100

    App completa di routing

--- src/app/country-detail/country-detail.component.ts
+++ src/app/country-detail/country-detail.component.ts
@@ -1,6 +1,9 @@
 import { Component, OnInit, Input } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { Location } from '@angular/common'
 
 import { Country } from 'app/models/country'
+import { CountryService } from 'app/services/country.service'
 
 @Component({
   selector: 'app-country-detail',
@@ -10,9 +13,23 @@ import { Country } from 'app/models/country'
 export class CountryDetailComponent implements OnInit {
   @Input() country: Country;
 
-  constructor() { }
+  constructor(
+    private route: ActivatedRoute,
+    private countryService: CountryService,
+    private location: Location
+  ) { }
 
   ngOnInit() {
+    this.getCountry();
   }
 
+  getCountry(): void{
+    const id = this.route.snapshot.paramMap.get('id');
+    this.countryService.getCountry(id)
+      .subscribe(country => this.country = country);
+  }
+
+  goBack() {
+    this.location.back();
+  }
 }
