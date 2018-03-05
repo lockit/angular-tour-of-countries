19ce3d504503b7e949a33c735a29ccbb99cfbe2b
Alessandro Locatelli <alessandro.locatelli@zero.it>
2018-03-05 21:14:11 +0100

    Styled dropdown - added other languages

--- src/app/globals.ts
+++ src/app/globals.ts
@@ -3,9 +3,16 @@
 
 export const defaultLanguage='it';
 
-export const languages= [
-  { id: 'en', name: 'english' },
-  { id: 'it', name: 'italiano' },
-  { id: 'br', name: 'brasileiro' },
-  { id: 'ja', name: '日本の' }
+export const languages=[
+  { id: 'it', name: 'Italiano' },
+  { id: 'de', name: 'Deutsch' },
+  { id: 'en', name: 'English' },
+  { id: 'es', name: 'Español' },
+  { id: 'fr', name: 'Français' },
+  { id: 'hr', name: 'Hrvatski jezik' },
+  { id: 'nl', name: 'Nederlands' },
+  { id: 'br', name: 'Português BR' },
+  { id: 'pt', name: 'Português PT' },
+  { id: 'fa', name: 'فارسی' },
+  { id: 'ja', name: '日本語 (にほんご)' }
 ];
